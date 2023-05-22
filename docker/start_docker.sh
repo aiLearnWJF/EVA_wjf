7e6e51dc44587bb3db8d9b6046854d66dff0b946
温建锋 <wenjianfeng@cloudwalk.com>
2023-05-22 14:08:27 +0800

    update

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,8 +1,13 @@
 # 提前下载好原始镜像，可去掉
-sudo docker pull pytorch/pytorch:1.9.0-cuda11.1-cudnn8-devel
+# sudo docker pull pytorch/pytorch:1.9.0-cuda11.1-cudnn8-devel
+# sudo docker load < pytorch_1.9.0-cuda11.1-cudnn8-devel.tar
 
+# 构建镜像名
 DST_IMAGE_NAME="eva_clip:1.0"
+# 构建容器名
 DST_CONTAINER_NAME="eva_clip_test"
+# shm-size
+SHM_SIZE=120G
 
 {
     sudo docker rm -f ${DST_CONTAINER_NAME} && \
@@ -18,12 +23,15 @@ DST_CONTAINER_NAME="eva_clip_test"
 }
 # 构建镜像
 sudo docker build -f Dockerfile -t ${DST_IMAGE_NAME} .
-
+echo "构建镜像成功"
 # 启动镜像
-# sudo docker run --name ${DST_CONTAINER_NAME} -it ${DST_IMAGE_NAME} pwd
+echo "开始启动容器并运行..."
+# 修改docker_cmd_bridge.sh控制分布式训练脚本
+# 如果需要后台运行 -it 改为 -d , 通过 docker logs -f 容器名 查看进展
 sudo docker run --name ${DST_CONTAINER_NAME} \
                 -p 18022:22 -p 18080:80 -p 8234:1234  \
                 -v/vehicle:/vehicle \
                 -v/data2/opensource/:/data2/opensource/  \
                 --gpus "all"  \
-                -it  ${DST_IMAGE_NAME}  bash /vehicle/yckj3860/code/EVA_wjf/docker_bridge.sh
+                --shm-size ${SHM_SIZE} \
+                -d  ${DST_IMAGE_NAME}  bash /vehicle/yckj3860/code/EVA_wjf/docker_cmd_bridge.sh

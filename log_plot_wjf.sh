11302ffcb1c7fad495212443b9fdec8b6d52995e
jeffery <1576850974@qq.com>
2023-06-01 08:58:13 +0000

    update

--- log_plot_wjf.sh
+++ log_plot_wjf.sh
@@ -17,15 +17,40 @@
 #     --ylims "0.5-1,0-1,0-4"
 
 # 0530 test
+# /mnt/pfs/data/yckj1563/miniconda3/envs/py37_torch1_7_evaclip/bin/python log_plot_wjf.py \
+#     --log_path  baiduyun/test_log/1.log \
+#         baiduyun/test_log/2.log \
+#         baiduyun/test_log/3.log \
+#         baiduyun/test_log/4.log \
+#         baiduyun/test_log/5.log \
+#         baiduyun/test_log/6.log \
+#         baiduyun/test_log/7.log \
+#         baiduyun/test_log/8.log \
+#         baiduyun/test_log/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_lr10_warm10_0_test.log \
+#         baiduyun/outputs/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_bs0_75_step500_0.log \
+#         baiduyun/test_log/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_ep21_test.log \
+#         baiduyun/test_log/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_lr50_step500_test.log \
+#         baiduyun/test_log/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_lr1000_lrv10_lrt10_step500_ep15_test.log \
+#     --index "imagenet-zeroshot-val-top1:,imagenet-zeroshot-val-top5" \
+#     --spilt_char "	, " \
+#     --strip_char " - " \
+#     --coff_index "0,1" \
+#     --line_coff "0,0" \
+#     --ylims "0.75-0.79,0.93-0.96" \
+#     --xlims "0-9,0-9" \
+#     --legend_labels "baseline,lr00_1,lr0_1_bs0_50,lr0_1_bs0_75,lr0_1_lrv001_lrt001,lr0_1,lrv_00_1_lrt_0_1,lrv_0_1_lrt_00_1,lr10_warm10_0_test,bs0_75_step500,baseline_ep21,lr50_step500,lr1000_lrv10_lrt10_step500_ep15" \
+#     --print_tb 1 \
+#     --print_tb_max_rows 9
+
+# 530 part
 /mnt/pfs/data/yckj1563/miniconda3/envs/py37_torch1_7_evaclip/bin/python log_plot_wjf.py \
-    --log_path  baiduyun/test_log/1.log \
+    --log_path baiduyun/test_log/ori_g14.log \
+        baiduyun/test_log/soup_g14_0601.log \
+        baiduyun/test_log/1.log \
         baiduyun/test_log/2.log \
         baiduyun/test_log/3.log \
         baiduyun/test_log/4.log \
-        baiduyun/test_log/5.log \
         baiduyun/test_log/6.log \
-        baiduyun/test_log/7.log \
-        baiduyun/test_log/8.log \
         baiduyun/test_log/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_lr10_warm10_0_test.log \
         baiduyun/outputs/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_bs0_75_step500_0.log \
         baiduyun/test_log/Eva_clip_0526_EVA01_CLIP_g_14_plus_psz14_s11B_baseline_ep21_test.log \
@@ -38,7 +63,7 @@
     --line_coff "0,0" \
     --ylims "0.75-0.79,0.93-0.96" \
     --xlims "0-9,0-9" \
-    --legend_labels "baseline,lr00_1,lr0_1_bs0_50,lr0_1_bs0_75,lr0_1_lrv001_lrt001,lr0_1,lrv_00_1_lrt_0_1,lrv_0_1_lrt_00_1,lr10_warm10_0_test,bs0_75_step500,baseline_ep21,lr50_step500,lr1000_lrv10_lrt10_step500_ep15" \
+    --legend_labels "ori_g14, soup_g14_0601, baseline,lr00_1,lr0_1_bs0_50,lr0_1_bs0_75,lr0_1,lr10_warm10_0_test,bs0_75_step500,baseline_ep21,lr50_step500,lr10_step500_ep15" \
     --print_tb 1 \
     --print_tb_max_rows 9
 
